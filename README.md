d8e0bf067e9acaacdf771757e934821abac49c45
Juho Jeong <aspectran@gmail.com>
2020-01-27 15:37:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+aspectran-aspectow
+===================
+
+Aspectow is a web application server on top of Aspectran 6, Undertow.
+
+## Running Aspectow under Aspectran Shell
+
+- Clone this repository
+
+  ```bash
+  $ git clone https://github.com/aspectran/aspectran-aspectow.git
+  ```
+
+- Build with Maven
+
+  ```bash
+  $ cd aspectran-aspectow
+  $ mvn clean package
+  ```
+
+- Startup the Aspectran Shell
+
+  ```bash
+  $ cd app/bin
+  $ ./shell.sh
+  ```
+
+- Access in your browser at http://localhost:8080
---
+++ setup/install-app.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+. ./app.conf
+
+echo "Installing application to $BASE_DIR ..."
+
+if [ ! -d "$REPO_DIR" ]; then
+  [ ! -d "$BUILD_DIR" ] && mkdir "$BUILD_DIR"
+  cd "$BUILD_DIR" || exit
+  git clone "$REPO_URL" "$APP_NAME"
+else
+  cd "$REPO_DIR" || exit
+  git pull origin master
+fi
+
+cp "$REPO_DIR/setup/app.conf" "$BASE_DIR" || exit
+cp "$REPO_DIR"/setup/scripts/*.sh "$BASE_DIR" || exit
+chmod +x "$BASE_DIR"/*.sh
+cp "$REPO_DIR/setup/install-service.sh" "$BASE_DIR/setup" || exit
+cp "$REPO_DIR/setup/uninstall-service.sh" "$BASE_DIR/setup" || exit
+chmod +x "$BASE_DIR"/setup/*.sh
+
+echo "--------------------------------------------------------------------------"
+echo "Your application installation is complete."
+echo "To register this application as a service, run the following script:"
+echo "  $BASE_DIR/setup/install-service.sh"
+echo "You can also remove a registered service by running the following script:"
+echo "  $BASE_DIR/setup/uninstall-service.sh"
+echo "--------------------------------------------------------------------------"
