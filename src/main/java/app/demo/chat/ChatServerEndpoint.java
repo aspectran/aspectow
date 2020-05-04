30eb6e006b255978fa26083bae7c10ad9da7a88b
Juho Jeong <aspectran@gmail.com>
2020-05-04 23:47:16 +0900

    Update

--- src/main/java/app/demo/chat/ChatServerEndpoint.java
+++ src/main/java/app/demo/chat/ChatServerEndpoint.java
@@ -68,7 +68,7 @@ public class ChatServerEndpoint extends ActivityContextAwareEndpoint {
             switch (payload.getType()) {
                 case CHAT:
                     if (username != null) {
-                        broadcastTextMessage(username, payload.getContent());
+                        broadcastTextMessage(session, username, payload.getContent());
                     }
                     break;
                 case JOIN:
@@ -135,7 +135,7 @@ public class ChatServerEndpoint extends ActivityContextAwareEndpoint {
     private void broadcastUserConnected(Session session, String username) {
         BroadcastConnectedUserPayload payload = new BroadcastConnectedUserPayload();
         payload.setUsername(username);
-        broadcast(session, new ChatMessage(payload));
+        broadcast(new ChatMessage(payload), session);
     }
 
     private void broadcastUserDisconnected(String username) {
@@ -144,11 +144,11 @@ public class ChatServerEndpoint extends ActivityContextAwareEndpoint {
         broadcast(new ChatMessage(payload));
     }
 
-    private void broadcastTextMessage(String username, String text) {
+    private void broadcastTextMessage(Session session, String username, String text) {
         BroadcastTextMessagePayload payload = new BroadcastTextMessagePayload();
         payload.setContent(text);
         payload.setUsername(username);
-        broadcast(new ChatMessage(payload));
+        broadcast(new ChatMessage(payload), session);
     }
 
     private void broadcastAvailableUsers() {
@@ -167,7 +167,7 @@ public class ChatServerEndpoint extends ActivityContextAwareEndpoint {
         }
     }
 
-    private void broadcast(Session ignoredSession, ChatMessage message) {
+    private void broadcast(ChatMessage message, Session ignoredSession) {
         synchronized (sessions) {
             for (Session session : sessions.values()) {
                 if (session.isOpen() && !session.getId().equals(ignoredSession.getId())) {
