894eee7c75c11407556334645291c67ad296a935
Juho Jeong <aspectran@gmail.com>
2025-03-07 21:43:15 +0900

    Update

--- src/main/java/aspectow/demo/common/db/BatchSqlSession.java
+++ src/main/java/aspectow/demo/common/db/BatchSqlSession.java
@@ -1,4 +1,4 @@
-package aspectow.demo.mybatis;
+package aspectow.demo.common.db;
 
 import com.aspectran.core.component.bean.annotation.Bean;
 import com.aspectran.core.component.bean.annotation.Component;
