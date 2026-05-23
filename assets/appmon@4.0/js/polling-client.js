e621cebc995783971e24bef65c22dfeee6a1fd81
Juho Jeong <aspectran@gmail.com>
2026-05-23 17:57:06 +0900

    Update

--- assets/appmon@4.0/js/polling-client.js
+++ assets/appmon@4.0/js/polling-client.js
@@ -1,131 +1,135 @@
 /*
- * Aspectow AppMon 4.0
- * Last modified: 2026-04-29
+ * Copyright (c) 2020-present The Aspectran Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /**
  * HTTP Polling implementation of the AppMon client.
+ *
+ * @version 4.0
+ * @last-modified 2026-05-22
  */
 class PollingClient extends BaseClient {
-    constructor(node, viewer, onJoined, onEstablished, onClosed, onFailed) {
-        super(node, viewer, onJoined, onEstablished, onClosed, onFailed);
-        this.endpointMode = "polling";
-        this.commands = [];
+    constructor(node, viewer, onSubscribed, onPrimary, onClosed, onFailed, isGatewayMode = false) {
+        super(node, viewer, onSubscribed, onPrimary, onClosed, onFailed, isGatewayMode);
+        this.pendingCommands = [];
         this.pollingTimer = null;
         this.stopped = false;
     }
 
-    start(appsToJoin) {
+    addClusterViewer(nodeId, viewer) {
+        this.clusterViewers[nodeId] = viewer;
+    }
+
+    addClusterNode(node, onSubscribed, onPrimary) {
+        this.clusterNodes[node.id] = {node, onSubscribed, onPrimary};
+    }
+
+    start(appsToSubscribe, nodeToSubscribe) {
         this.stopped = false;
-        this.join(appsToJoin);
+        this.nodeToSubscribe = nodeToSubscribe;
+        this.appsToSubscribe = appsToSubscribe;
+        this.connect(this.node.id);
     }
 
     stop() {
         this.stopped = true;
+        this.primary = false;
         if (this.pollingTimer) {
             clearTimeout(this.pollingTimer);
             this.pollingTimer = null;
         }
     }
 
-    speed(speed) {
-        this.changePollingInterval(speed);
-    }
-
-    refresh(options) {
-        let cmdOptions = ["command:refresh"];
-        if (options) {
-            cmdOptions.push(...options);
-        }
-        this.sendCommand(cmdOptions);
-    }
-
-    sendCommand(options) {
-        if (options) {
-            options.forEach(option => this.withCommand(option));
-        }
-    }
-
-    withCommand(command) {
-        if (!this.commands.includes(command)) {
-            this.commands.push(command);
-        }
-    }
-
-    join(appsToJoin) {
+    connect(nodeId) {
         $.ajax({
-            url: this.node.endpoint.path + "/appmon/polling/join",
+            url: this.node.endpoint.path + "/appmon/polling/subscribe",
             type: "post",
             dataType: "json",
             data: {
+                nodeId: nodeId,
                 timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-                apps: appsToJoin
+                nodeToSubscribe: this.nodeToSubscribe,
+                appsToSubscribe: this.appsToSubscribe
             },
             success: (data) => {
                 if (data) {
-                    this.retryCount = 0;
-                    this.node.endpoint['mode'] = this.endpointMode;
-                    this.node.endpoint['pollingInterval'] = data.pollingInterval;
-                    if (this.onJoined) {
-                        this.onJoined(this.node, data);
+                    if (data.primary && !data.appsToSubscribe) {
+                        console.warn("No verified apps found. Please check the configuration of the backend.");
+                        return;
                     }
-                    if (this.onEstablished) {
-                        this.onEstablished(this.node);
+
+                    if (data.primary) {
+                        this.retryCount = 0;
+                        this.node.endpoint['mode'] = "polling";
+                        this.node.endpoint['pollingInterval'] = data.pollingInterval;
                     }
-                    this.viewer.printMessage("Polling every " + data.pollingInterval + " milliseconds.");
-                    this.polling(appsToJoin);
+
+                     this.establish(data.nodeId, data.primary, data.alive);
+
+                    if (this.primary && !this.stopped) {
+                        this.appsToSubscribe = data.appsToSubscribe;
+                        this.poll();
+                    }
                 } else {
                     console.log(this.node.id, "connection failed");
                     this.viewer.printErrorMessage("Connection failed.");
-                    this.rejoin(appsToJoin);
+                    this.reconnect();
                 }
             },
             error: (xhr, status, error) => {
                 console.log(this.node.id, "connection failed", error);
                 this.viewer.printErrorMessage("Connection failed.");
-                this.rejoin(appsToJoin);
+                this.reconnect();
             }
         });
     }
 
-    polling(appsToJoin) {
+    poll() {
         if (this.stopped) return;
-        let withCommands = null;
-        if (this.commands.length) {
-            withCommands = this.commands.slice();
-            this.commands.length = 0;
+        let commands = null;
+        if (this.pendingCommands.length) {
+            commands = this.pendingCommands.slice();
+            this.pendingCommands.length = 0;
         }
         $.ajax({
             url: this.node.endpoint.path + "/appmon/polling/pull",
-            type: "get",
+            type: "post",
             cache: false,
-            data: withCommands ? {
-                commands: withCommands
+            data: commands ? {
+                "commands[]": commands
             } : null,
             success: (data) => {
                 if (this.stopped) return;
                 if (data && data.messages) {
-                    data.messages.forEach(msg => this.viewer.processMessage(msg));
+                    this.processMessages(data.messages);
                     this.pollingTimer = setTimeout(() => {
-                        this.polling(appsToJoin);
+                        this.poll();
                     }, this.node.endpoint.pollingInterval);
                 } else {
                     console.log(this.node.id, "connection lost");
                     this.viewer.printErrorMessage("Connection lost.");
-                    if (this.onClosed) {
-                        this.onClosed(this.node);
-                    }
-                    this.rejoin(appsToJoin);
+                    if (this.onClosed) this.onClosed(this.node);
+                    this.reconnect();
                 }
             },
             error: (xhr, status, error) => {
                 if (this.stopped) return;
                 console.log(this.node.id, "connection lost", error);
                 this.viewer.printErrorMessage("Connection lost.");
-                if (this.onClosed) {
-                    this.onClosed(this.node);
-                }
-                this.rejoin(appsToJoin);
+                if (this.onClosed) this.onClosed(this.node);
+                this.reconnect();
             }
         });
     }
@@ -151,5 +155,67 @@ class PollingClient extends BaseClient {
                 this.viewer.printMessage("Failed to change polling interval.");
             }
         });
+    }
+
+    processMessages(messages) {
+        if (messages) {
+            messages.forEach(msg => {
+                const idx = msg.indexOf(':');
+                if (idx === -1) return;
+
+                const nodeId = msg.substring(0, idx);
+                const message = msg.substring(idx + 1);
+
+                if (this.primary) {
+                    // Data messages
+                    const viewer = this.getViewer(nodeId);
+                    if (viewer) {
+                        viewer.processMessage(message);
+                    } else {
+                        console.warn("No viewer registered for nodeId:", nodeId, "Message:", message);
+                    }
+                } else {
+                    console.error("Unexpected message received before primary connection established:", message);
+                }
+            });
+        }
+    }
+
+    establish(nodeId, primary, alive) {
+        if (primary) {
+            this.primary = true;
+            this.primaryNodeId = nodeId;
+        }
+
+        const config = this.getNodeConfig(nodeId);
+        if (config) {
+            config.node.alive = !!alive;
+            if (config.onSubscribed && !config.node.subscribed) {
+                config.onSubscribed(config.node);
+            }
+        }
+
+        const viewer = this.getViewer(nodeId);
+        if (!alive) {
+            viewer.printMessage("Node '" + nodeId + "' not alive");
+        } else {
+            viewer.printMessage("Polling every " + this.node.endpoint.pollingInterval + " milliseconds.");
+        }
+        if (primary) {
+            if (config && config.onPrimary) config.onPrimary(config.node);
+            this.sendCommand(["command:established"], nodeId);
+        }
+    }
+
+    sendCommand(options, nodeId) {
+        if (options) {
+            let arr = options.slice();
+            arr.push("nodeId:" + (nodeId || this.primaryNodeId));
+            const cmd = arr.join(";");
+            console.log("send", cmd);
+            if (!this.pendingCommands.includes(cmd)) {
+                this.pendingCommands.push(cmd);
+            }
+        }
     }
 }
--- assets/appmon@4.0/js/websocket-client.js
+++ assets/appmon@4.0/js/websocket-client.js
@@ -1,119 +1,138 @@
 /*
- * Aspectow AppMon 4.0
- * Last modified: 2026-04-29
+ * Copyright (c) 2020-present The Aspectran Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /**
  * WebSocket implementation of the AppMon client.
+ * In Gateway Mode, it manages a single physical connection for the entire cluster.
+ *
+ * @version 4.0
+ * @last-modified 2026-05-22
  */
 class WebsocketClient extends BaseClient {
-    constructor(node, viewer, onJoined, onEstablished, onClosed, onFailed) {
-        super(node, viewer, onJoined, onEstablished, onClosed, onFailed);
-        this.endpointMode = "websocket";
+    constructor(node, viewer, onSubscribed, onPrimary, onClosed, onFailed, isGatewayMode) {
+        super(node, viewer, onSubscribed, onPrimary, onClosed, onFailed, isGatewayMode);
         this.heartbeatInterval = 5000;
-        this.socket = null;
         this.heartbeatTimer = null;
+        this.socket = null;
         this.pendingMessages = [];
-        this.established = false;
     }
 
-    start(appsToJoin) {
-        this.openSocket(appsToJoin);
+    start(appsToSubscribe, nodeToSubscribe) {
+        this.nodeToSubscribe = nodeToSubscribe;
+        this.appsToSubscribe = appsToSubscribe;
+        this.openSocket();
     }
 
     stop() {
         this.closeSocket();
     }
 
-    refresh(options) {
-        let cmdOptions = ["command:refresh"];
-        if (options) {
-            cmdOptions.push(...options);
-        }
-        this.sendCommand(cmdOptions);
-    }
-
-    sendCommand(options) {
-        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-            this.socket.send(options ? options.join(";") : "");
-        }
-    }
-
-    openSocket(appsToJoin) {
+    openSocket() {
         this.closeSocket(false);
         const url = new URL(this.node.endpoint.path + "/appmon/websocket/" + this.node.endpoint.token, location.href);
         url.protocol = url.protocol.replace("https:", "wss:").replace("http:", "ws:");
 
+        console.log("connecting to websocket:", url.href);
         this.socket = new WebSocket(url.href);
 
         this.socket.onopen = () => {
-            console.log(this.node.id, "socket connected:", this.node.endpoint.path);
+            console.log(this.node.id, "socket connected");
             this.pendingMessages.push("Socket connection successful");
-            const options = [
-                "command:join",
-                "timeZone:" + Intl.DateTimeFormat().resolvedOptions().timeZone
-            ];
-            if (appsToJoin) {
-                options.push("appsToJoin:" + appsToJoin);
-            }
-            this.socket.send(options.join(";"));
-            this.heartbeatPing();
+            
+            // Connect to the first node
+            this.connect(this.node.id);
+            this.sendPing();
             this.retryCount = 0;
         };
 
         this.socket.onmessage = (event) => {
-            if (typeof event.data === "string") {
-                const msg = event.data;
-                if (this.established) {
-                    if (msg.startsWith("pong:")) {
-                        this.node.endpoint.token = msg.substring(5);
-                        this.heartbeatPing();
-                    } else {
-                        this.viewer.processMessage(msg);
-                    }
-                } else if (msg.startsWith("joined:")) {
-                    console.log(this.node.id, msg, this.node.endpoint.token);
-                    const payload = (msg.length > 7 ? JSON.parse(msg.substring(7)) : null);
-                    this.establish(payload);
+            if (typeof event.data !== "string") return;
+            const msg = event.data;
+            const idx = msg.indexOf(':');
+            if (idx === -1) {
+                console.warn("Invalid message format received:", msg);
+                return;
+            }
+
+            const nodeId = msg.substring(0, idx);
+            const message = msg.substring(idx + 1);
+
+            if (this.primary) {
+                // Standard control messages
+                if (message.startsWith(":pong:")) {
+                    this.node.endpoint.token = message.substring(6);
+                    this.sendPing();
+                    return;
                 }
+
+                // Control messages in Gateway Mode
+                if (this.isGatewayMode && message.startsWith(":subscribed:")) {
+                    const alive = (message === ":subscribed:alive");
+                    this.establish(nodeId, false, alive);
+                    return;
+                }
+
+                // Data messages
+                const viewer = this.getViewer(nodeId);
+                if (viewer) {
+                    viewer.processMessage(message);
+                } else {
+                    console.warn("No viewer registered for nodeId:", nodeId, "Message:", message);
+                }
+            } else if (message.startsWith(":subscribed:")) {
+                const primary = message.startsWith(":subscribed:primary:");
+                const alive = message.endsWith(":alive");
+                this.establish(nodeId, primary, alive);
+            } else {
+                console.error("Unexpected message received before primary connection established:", message);
             }
         };
 
         this.socket.onclose = (event) => {
             this.closeSocket(true);
-            if (this.node.endpoint.mode === this.endpointMode) {
-                if (this.onClosed) {
-                    this.onClosed(this.node);
-                }
-                if (event.code === 1003) {
-                    console.log(this.node.id, "socket connection refused: ", event.code);
-                    this.viewer.printErrorMessage("Socket connection refused by server.");
-                    return;
-                }
-                if (event.code === 1000 || this.retryCount === 0) {
-                    console.log(this.node.id, "socket connection closed: ", event.code);
-                    this.viewer.printMessage("Socket connection closed.");
-                }
-                if (event.code !== 1000) {
-                    this.rejoin(appsToJoin);
-                }
+            if (this.onClosed) this.onClosed(this.node);
+            if (event.code === 1003) {
+                console.warn("Websocket connection refused: ", event.code);
+                this.viewer.printErrorMessage("Socket connection refused by server.");
+                return;
+            }
+            if (event.code === 1011) {
+                console.log("Websocket connection closed: ", event.code);
+                this.viewer.printErrorMessage("Websocket connection closed due to server error.");
+                return;
+            }
+            if (event.code === 1000 || this.retryCount === 0) {
+                console.log("Websocket connection closed: ", event.code);
+                this.viewer.printMessage("Websocket connection closed.");
+            }
+            if (event.code !== 1000) {
+                setTimeout(() => this.reconnect(), 1000);
             }
         };
 
         this.socket.onerror = (event) => {
-            if (this.node.endpoint.mode === this.endpointMode) {
-                console.log(this.node.id, "websocket error:", event);
-                this.viewer.printErrorMessage("Could not connect to the WebSocket server.");
-            }
-            if (this.onFailed) {
-                this.onFailed(this.node);
-            }
+            console.error(this.node.id, "websocket error:", event);
+            this.viewer.printErrorMessage("Could not connect to the WebSocket server.");
+            if (this.onFailed) this.onFailed(this.node);
         };
     }
 
     closeSocket(afterClosing) {
         if (this.socket) {
-            this.established = false;
+            this.primary = false;
             if (!afterClosing) {
                 this.socket.close();
             }
@@ -125,25 +144,59 @@ class WebsocketClient extends BaseClient {
         }
     }
 
-    establish(payload) {
-        this.node.endpoint['mode'] = this.endpointMode;
-        if (this.onJoined) {
-            this.onJoined(this.node, payload);
+    connect(nodeId) {
+        const options = ["command:subscribe"];
+        options.push("timeZone:" + Intl.DateTimeFormat().resolvedOptions().timeZone);
+        if (this.nodeToSubscribe) {
+            options.push("nodeToSubscribe:" + this.nodeToSubscribe);
         }
-        while (this.pendingMessages.length) {
-            this.viewer.printMessage(this.pendingMessages.shift());
+        if (this.appsToSubscribe) {
+            options.push("appsToSubscribe:" + this.appsToSubscribe);
         }
-        if (this.onEstablished) {
-            this.onEstablished(this.node);
-        }
-        while (this.pendingMessages.length) {
-            this.viewer.printMessage(this.pendingMessages.shift());
-        }
-        this.established = true;
-        this.socket.send("command:established");
+        this.sendCommand(options, nodeId);
     }
 
-    heartbeatPing() {
+    establish(nodeId, primary, alive) {
+        if (primary) {
+            this.primary = true;
+            this.primaryNodeId = nodeId;
+        }
+
+        const config = this.getNodeConfig(nodeId);
+        if (config) {
+            config.node.alive = !!alive;
+            if (config.onSubscribed && !config.node.subscribed) {
+                config.onSubscribed(config.node);
+            }
+        }
+
+        const viewer = this.getViewer(nodeId);
+        if (primary) {
+            if (config && config.onPrimary) config.onPrimary(config.node);
+            while (this.pendingMessages.length) {
+                viewer.printMessage(this.pendingMessages.shift());
+            }
+            this.sendCommand([
+                "command:established",
+                "nodeToSubscribe:" + (this.nodeToSubscribe || "")
+            ], nodeId);
+        }
+        if (!alive) {
+            viewer.printErrorMessage("Node " + nodeId + " not alive");
+        }
+    }
+
+    sendCommand(options, nodeId) {
+        if (options && this.socket && this.socket.readyState === WebSocket.OPEN) {
+            const arr = options.slice();
+            arr.push("nodeId:" + (nodeId || this.primaryNodeId));
+            const cmd = arr.join(";");
+            console.log("send", cmd);
+            this.socket.send(cmd);
+        }
+    }
+
+    sendPing() {
         if (this.heartbeatTimer) {
             clearTimeout(this.heartbeatTimer);
         }
